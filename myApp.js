b67210ca220b36195f6215e81ebe05cfe6052fa7
Ivan <ivandasilvamartin1@gmail.com>
2024-01-22 19:42:43 +0000

    arreglo nombre

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-let mongoose = require('moongose')
+let mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
